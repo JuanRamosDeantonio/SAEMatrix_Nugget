6454d315a778ee4d0846f6bb4eb72cfb09281766
Oscar Alexander Betancourt Garcia <beta_ga@hotmail.com>
2023-02-14 12:46:23 -0500

    Se agrega clase contenedora de las inyecciones

---
+++ SaeMatrix.Common/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaeMatrix.Common.Managers.Implementations;
+using SaeMatrix.Common.Managers.Interfaces;
+
+namespace SAE.Matrix.Common
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Asset repositories
+            services.AddScoped<IEmailManager, EmailManager>();
+
+            return services;
+        }
+    }
+}
