9f8bec5b5d35fba8cc9a89d0ac211403f89d8d58
Oscar Alexander Betancourt Garcia <beta_ga@hotmail.com>
2023-03-26 20:08:16 -0500

    Se agregan clases para la paginación.

---
+++ SaeMatrix.Common/Entities/PaginationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SAE.Matrix.Common.Entities
+{
+    public class PaginationRequest<T>
+    {
+        public int Page { get; set; }
+        public int Size { get; set; }
+        public T Filter { get; set; }
+    }
+}
---
+++ SaeMatrix.Common/Entities/PaginationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SAE.Matrix.Common.Entities
+{
+    public class PaginationResponse<T>
+    {
+        public int TotalRecords { get; set; }
+        public T Result { get; set; }
+    }
+}
